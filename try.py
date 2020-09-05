a392ef426bee9e1bff74050851cb80281ff22167
nela <anisoara.ionela@gmail.com>
2020-09-05 22:34:54 +0300

    add buttons and file copy

--- try.py
+++ try.py
@@ -1,43 +1,76 @@
 from tkinter import filedialog
+import shutil
 import tkinter as tk
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
+        self.grid()
         self.create_widgets()
 
     def create_widgets(self):
         # hi there
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        # self.hi_there = tk.Button(self)
+        # self.hi_there["text"] = "Hello World\n(click me)"
+        # self.hi_there["command"] = self.say_hi
+        # self.hi_there["width"] = "20"
+        # self.hi_there.grid(row=0, column=0,
+        #                       padx=10, pady=10, columnspan=2, rowspan=2,)
+
+        # exit
+        # self.quit = tk.Button(self, text="QUIT", fg="red",
+        #                       command=self.master.destroy)
+        # self.quit.grid(row=0, column=10, padx=10, pady=10)
+        
+        # label
+        self.lbl = tk.Label(text="Hello, you haven't chosen a file yet")
+        self.lbl.grid(row=1, column=0, padx=10, pady=10)
+
         # file piker
         self.file_pik = tk.Button(self)
         self.file_pik["text"] = "Choose a file"
         self.file_pik["command"] = self.file_chooser
-        self.file_pik.pack(side="bottom")
+        self.file_pik["width"] = "20"
+        self.file_pik.grid(row=5, column=0, padx=10, pady=10)
 
-        # exit
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        # file saver
+        self.file_save = tk.Button(self)
+        self.file_save["text"] = "Save as"
+        self.file_save["command"] = self.file_saver
+        self.file_save["width"] = "20"
+        self.file_save.grid(row=7, column=0, padx=10, pady=10)
 
     def say_hi(self):
         print("hi there, everyone!")
 
     def file_chooser(self):
-        fname = filedialog.askopenfilename(initialdir='.',
-                                            title='Select file', 
-                                            filetypes=(('Audit files', '*.audit'), ('all files', '*.*')))
-        print(fname)
+        title = "seleeect"
+        filetypes = (('Audit files', '*.audit'), ('all files', '*.*'))
+        self.fname = filedialog.askopenfilename(initialdir='.',
+                                           title=title,
+                                           filetypes=filetypes)
+        
+        file_name = self.fname.split('/')[-1]
+        self.lbl.configure(text=f"You chose\n{file_name}")
+        print(self.fname)
+        
+
+    def file_saver(self):
+        filename = filedialog.asksaveasfilename(initialdir = ".", title = "Select folder", defaultextension=".audit")
+        if filename is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+            return
+
+        shutil.copy2(self.fname, filename)
+        # my_file = open(filename, "w")
+        # my_file = self.fname
+        # my_file.close() 
+        # filename = 
+        # print (filename)
 
 
 root = tk.Tk(className='Security app')
 root.geometry("1000x500")
-# root.filename =  filedialog.askopenfilename( initialdir = "/", title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
 
 app = Application(master=root)
 app.mainloop()
